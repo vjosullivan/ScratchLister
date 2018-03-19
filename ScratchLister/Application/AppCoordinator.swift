fd2caf2d386106cc6e01ee43d5daf6d26d816001
Vince O'Sullivan (from laptop) <vjosullivan@gmail.com>
2018-03-19 06:47:11 +0000

    Add AppCoordinator

---
+++ ScratchLister/Application/AppCoordinator.swift
@@ -0,0 +1,22 @@
+//
+//  AppCoordinator.swift
+//  ScratchLister
+//
+//  Created by Vincent O'Sullivan on 19/03/2018.
+//  Copyright © 2018 Vincent O'Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class AppCoordinator {
+
+    private var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    public func start() {
+        
+    }
+}
